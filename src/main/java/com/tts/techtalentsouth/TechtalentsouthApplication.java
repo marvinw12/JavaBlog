bef30d0b0b74efbe1d655db4c839a121d9317977
Marvin W <marvwood12@gmail.com>
2020-10-21 09:29:37 -0400

    init commit

---
+++ src/main/java/com/tts/techtalentsouth/TechtalentsouthApplication.java
@@ -0,0 +1,13 @@
+package com.tts.techtalentsouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtalentsouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtalentsouthApplication.class, args);
+	}
+
+}
