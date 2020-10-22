2d9dc90797d67402e05e48e8a77ebcd7be7d6cc1
Marvin W <marvwood12@gmail.com>
2020-10-21 22:23:57 -0400

    added additional GetMapping method

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.tts.techtalentblog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,20 +10,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+
 @Controller
 public class BlogPostController {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
+
+    private List<BlogPost> posts = new ArrayList<>();
     
     @GetMapping
-    public String index(BlogPost blogPost){
+    public String index(BlogPost blogPost, Model model){
+        model.addAttribute("posts", posts);
         return "blogpost/index";
     }
 
-    @PostMapping(value="/")
+    @GetMapping(value = "/blogpost/new")
+    public String newBlog(BlogPost blogPost){
+        return "blogpost/new";
+    }
+
+    @PostMapping(value="/blogpost")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
         blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        posts.add(blogPost);
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
